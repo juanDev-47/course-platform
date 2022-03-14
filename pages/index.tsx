af288053d2889037288da7bc5feb31affb4ed8f9
Alejandro Montoya <alejandrom.montoya@udea.edu.co>
2022-03-14 18:15:54 -0500

    restore index

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,14 +13,6 @@ const Home = () => {
       <h1 className='text-3xl text-blue-300 font-bold underline'>
         Hello world! ;
       </h1>
-      <table
-        tittles={[
-          { title: 'hola', keyCol: 'hello', customClass: 'col-span-3' },
-          { title: 'mundo', keyCol: 'world' },
-        ]}
-        data={[]}
-        colsClass='grid-cols-5'
-      />
     </div>
   );
 }
