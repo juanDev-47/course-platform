cf19407985f0e59fd791ab9c7a033c6fca918b21
juanDev-47 <juan1037av@gmail.com>
2022-03-14 23:30:08 -0500

    se aceptan cambios desde main

--- config/prisma.ts
+++ config/prisma.ts
@@ -1,21 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
-<<<<<<< HEAD
-    var prismaGlobal: PrismaClient
-}
-
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if(!global.prismaGlobal) {
-        global.prismaGlobal = new PrismaClient();
-    }
-    prisma = global.prismaGlobal;
-}
-
-=======
   var prismaGlobal: PrismaClient;
 }
 
@@ -29,5 +14,4 @@ if (process.env.NODE_ENV === 'production') {
   }
   prisma = global.prismaGlobal;
 }
->>>>>>> main
 export default prisma;
