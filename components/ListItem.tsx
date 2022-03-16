07a600be4d5bb03f1ccedbae120ddfeda8a95172
Santiago <santiagomarinocampo@gmail.com>
2022-03-16 12:26:11 -0500

    Correcion pull request

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,6 +2,8 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import { Course } from 'interfaces/TrainingPlan';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 type Props = {
   item: any;
@@ -11,7 +13,6 @@ type Props = {
 
 const ListItem = ({ item, onClick, index }: Props) => {
   const typeItem = typeof item;
-  console.log(typeItem, 'aaa');
   let itemComponent;
   const aux = true;
 
@@ -43,14 +44,11 @@ export default ListItem;
 
 const ListItemCourse = ({ item }: any) => (
   <div className='flex justify-between px-2 py-2 bg-gray-300 rounded-lg border-2 cursor-pointer hover:border-indigo-500'>
-    <p className='flex text-gray-700'>
-      <svg
-        className='h2 w-2 text-indigo-500 mx-2'
-        viewBox='0 0 8 8'
-        fill='currentColor'
-      >
-        <circle cx='4' cy='4' r='3' />
-      </svg>
+    <p className='flex text-gray-700 items-center flex-row gap-3'>
+      <FontAwesomeIcon
+        icon={faCircle}
+        className='h-2 w-2 text-indigo-500 mx-2'
+      />
       {item.name}
     </p>
     <p className='text-gray-500 font-thin'>{item.platform}</p>
--- pages/trainingPlans/index.tsx
+++ pages/trainingPlans/index.tsx
@@ -1,9 +1,36 @@
+import Table from '@components/Table';
 import React from 'react';
 
-export default function Home() {
-  return (
-    <h1 className='text-3xl text-blue-300 font-bold underline'>
-      desde training plans ;
-    </h1>
-  );
-}
+const index = () => (
+  <Table
+    tittles={[
+      {
+        title: 'col1',
+        keyCol: 'col1',
+      },
+      {
+        title: 'col2',
+        keyCol: 'col2',
+      },
+      {
+        title: 'col3',
+        keyCol: 'col3',
+      },
+      {
+        title: 'col4',
+        keyCol: 'col4',
+      },
+    ]}
+    data={[
+      {
+        id: '1',
+        col1: 'Col1',
+        col2: 'col2',
+        col3: 'col3',
+        col4: 'col4',
+      },
+    ]}
+  />
+);
+
+export default index;
