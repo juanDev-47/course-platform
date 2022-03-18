2ee23f54d0b1f49565eb8efd626ec64a29425bbc
Santiago <santiagomarinocampo@gmail.com>
2022-03-17 20:22:12 -0500

    design table

--- pages/training-plans/index.tsx
+++ pages/training-plans/index.tsx
@@ -1,36 +1,5 @@
-import Table from '@components/Table';
 import React from 'react';
 
-const index = () => (
-  <Table
-    tittles={[
-      {
-        title: 'col1',
-        keyCol: 'col1',
-      },
-      {
-        title: 'col2',
-        keyCol: 'col2',
-      },
-      {
-        title: 'col3',
-        keyCol: 'col3',
-      },
-      {
-        title: 'col4',
-        keyCol: 'col4',
-      },
-    ]}
-    data={[
-      {
-        id: '1',
-        col1: 'Col1',
-        col2: 'col2',
-        col3: 'col3',
-        col4: 'col4',
-      },
-    ]}
-  />
-);
+const index = () => {};
 
 export default index;
