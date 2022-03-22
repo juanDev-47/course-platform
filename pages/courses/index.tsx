f858844745e0ee912e123f46ef08a8778eaac420
juanDev-47 <juan1037av@gmail.com>
2022-03-22 16:21:41 -0500

    configurando table courses

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -2,8 +2,9 @@ import { useQuery } from '@apollo/client';
 import Table from '@components/Table';
 import { GET_COURSES_FORMTRAINIGPLAN } from 'graphql/queries/course';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { matchRoles } from 'utils/matchRoles';
+import { Course } from 'interfaces/TrainingPlan';
 
 export async function getServerSideProps(context: any) {
   const props = await matchRoles(context);
@@ -12,36 +13,59 @@ export async function getServerSideProps(context: any) {
   };
 }
 
-const index =  () => {
+const index = () => {
 
+  const [availableCourses, setAvailableCourses] = useState<Course[]>([]);
+  
   // query for bring the courses
-  // const { data, loading } = useQuery(GET_COURSES_FORMTRAINIGPLAN, {
-  //   fetchPolicy: 'cache-and-network',
-  // });
+  const { data, loading } = useQuery(GET_COURSES_FORMTRAINIGPLAN, {
+    fetchPolicy: 'cache-and-network',
+  });
 
+  // datos convertidos para mostrar en la tabla
+  let dateForm: [] = [];
+
+
+  useEffect(() => {
+    if (data) {
+      setAvailableCourses(data.getCourses);
+    }
+  }, [data]);
+
+  // 
+  let dataForm = availableCourses.map(element => {
+    let item = {
+      id: element.id,
+      col1: element.name,
+      col2: element.hours,
+      col3: element.platform
+    }
+    return item
+  });
+  
+  console.log(availableCourses)
+
+  // instance of useRouter for send params throught paths
   const router = useRouter();
   const onCreate = () => {
     router.push('/courses/create-course');
   }
-
-  const data = [{id: "kdjhfksd",
-          col1: "course 1",
-          col2: 3,
-          col3: "Youtube",
-          }]
   
   const deleteCourse = (id: String) => {
     
   }
 
   
-  const editCourse = (id: String) => {
-    router.push('/courses/create-course');
+  const editCourse = (id: string) => {
+    router.push({
+      pathname: '/courses/create-course',
+      query: { keyWord: id}
+    });
   }
 
-  // console.log(courses);
+  // console.log(courses);  actionsContext={ActionsContext}
   return (
-    <Table data={data} title='Courses' tittles={[
+    <Table  data={dataForm} title='Courses' tittles={[
       {
         title: 'Name',
         keyCol: 'col1',
